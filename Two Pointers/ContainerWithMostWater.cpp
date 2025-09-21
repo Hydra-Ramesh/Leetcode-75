737c2f509eb177a03375b23c52f747a7e9447415
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-09-22 01:14:49 +0530

    Add Leetcode 11

---
+++ Two Pointers/ContainerWithMostWater.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0;
+        int j = height.size() - 1;
+        int maxArea = 0;
+
+        while (i < j) {
+            int area = min(height[i], height[j]) * (j - i);
+            maxArea = max(maxArea, area);
+
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+
+    int result = sol.maxArea(height);
+    cout << "Maximum area: " << result << endl;
+
+    return 0;
+}
+
